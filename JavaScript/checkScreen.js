cc91cbd25b453a277f204dc8344ed3f50449a586
LWeisenhorn <lweisenh@uncc.edu>
2023-02-15 18:23:18 -0500

    Added js alert for smaller screens

---
+++ JavaScript/checkScreen.js
@@ -0,0 +1,6 @@
+function checkScreen() {
+    if(window.screen.availWidth < 1000 || window.screen.availHeight < 700) {
+        alert("This website has not been optimized for this device yet. Please access this website on a desktop or full-screen for a better experience.");           
+    }
+    
+}
