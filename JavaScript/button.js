98afdd2dc9c6edde1c71d132227514c06a573fc3
LWeisenhorn <lweisenh@uncc.edu>
2023-06-19 22:16:48 -0400

    Added mobile scaling, fixed broken imgs

--- JavaScript/button.js
+++ JavaScript/button.js
@@ -3,7 +3,7 @@ function showLangs() {
     document.getElementById("frameworkButton").className = "toolButton";
     document.getElementById("otherButton").className = "toolButton";
 
-    document.getElementById("toolsFlexBox").innerHTML = '<div><img src="images/icons/python.png" alt="Python logo" width="60px"><p>Python</p></div><div><img src="images/icons/javascript.png" alt="JavaScript logo" width="60px"><p>JavaScript</p></div><div><img src="images/icons/c.png" alt="C logo" width="60px"><p>C</p></div><div><img src="images/icons/c++.png" alt="C++ logo" width="60px"><p>C++</p></div><div><img src="images/icons/java.png" alt="Java logo" width="60px"><p>Java</p></div><div><img src="images/icons/database.png" alt="Database logo" width="60px"><p>NoSQL/MySQL</p></div>'
+    document.getElementById("toolsFlexBox").innerHTML = '<div><img src="images/icons/python.png" alt="Python logo" width="60vw"><p>Python</p></div><div><img src="images/icons/javascript.png" alt="JavaScript logo" width="60vw"><p>JavaScript</p></div><div><img src="images/icons/c.png" alt="C logo" width="60vw"><p>C</p></div><div><img src="images/icons/java.png" alt="Java logo" width="60vw"><p>Java</p></div><div><img src="images/icons/database.png" alt="Database logo" width="60vw"><p>SQL</p></div>'
 }
 
 function showFrameworks() {
@@ -11,7 +11,7 @@ function showFrameworks() {
     document.getElementById("frameworkButton").className = "active toolButton";
     document.getElementById("otherButton").className = "toolButton";
 
-    document.getElementById("toolsFlexBox").innerHTML = '<div><img src="images/icons/react.png" alt="React logo" width="60px"><p>React</p></div><div><img src="images/icons/nodejs.png" alt="nodeJS logo" height="60px"><p>Node.js</p></div> <div><img src="images/icons/mongodb.png" alt="mongoDB logo" height="60px"><p>MongoDB</p></div><div><img src="images/icons/flask.png" alt="Flask logo" height="60px"><p>Flask</p></div>';
+    document.getElementById("toolsFlexBox").innerHTML = '<div><img src="images/icons/react.png" alt="React logo" width="60vw"><p>React</p></div><div><img src="images/icons/nodejs.png" alt="nodeJS logo" height="60vw"><p>Node.js</p></div> <div><img src="images/icons/mongodb.png" alt="mongoDB logo" height="60vw"><p>MongoDB</p></div><div><img src="images/icons/flask.png" alt="Flask logo" height="60vw"><p>Flask</p></div>';
 }
 
 function showOther() {
@@ -19,5 +19,5 @@ function showOther() {
     document.getElementById("frameworkButton").className = "toolButton";
     document.getElementById("otherButton").className = "active toolButton";
 
-    document.getElementById("toolsFlexBox").innerHTML = '<div><img src="images/icons/figma.png" alt="Figma logo" width="60px"><p>Figma</p></div><div><img src="images/icons/cad.png" alt="CAD logo" height="60px"><p>CAD</p></div><div><img src="images/icons/microsoft.png" alt="Microsoft Office logo" width="60px"><p>Microsoft Office</p></div>';
+    document.getElementById("toolsFlexBox").innerHTML = '<div><img src="images/icons/figma.png" alt="Figma logo" width="60vw"><p>Figma</p></div><div><img src="images/icons/cad.png" alt="CAD logo" height="60vw"><p>CAD</p></div><div><img src="images/icons/microsoft.png" alt="Microsoft Office logo" width="60vw"><p>Microsoft Office</p></div>';
 }
--- JavaScript/checkScreen.js
+++ JavaScript/checkScreen.js
@@ -1,6 +1,8 @@
 function checkScreen() {
     if(window.screen.availWidth < 1000 || window.screen.availHeight < 700) {
-        alert("This website has not been optimized for this device yet. Please access this website on a desktop or full-screen for a better experience.");           
+        document.getElementsByClassName("overlay")[0].innerHTML = "<a href='BoFA.html'><p style='margin-left: 3vw;'>BoFA Project</p></a>";
+        document.getElementsByClassName("overlay")[1].innerHTML = "<a href='fileSorter.html'><p style='margin-left: 3vw;'>File Sorter</p></a>";
+        document.getElementsByClassName("overlay")[2].innerHTML = "<a href='logicGate.html'><p style='margin-left: 3vw;'>Logic Gates</p></a>";
     }
     
 }
